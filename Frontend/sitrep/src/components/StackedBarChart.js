b72883a8982c3155d8cc00009d216f794fb57d73
demotri03 <bolf.laci97@gmail.com>
2022-07-28 20:01:04 +0200

    implement random data mocking for barcharts on page reload

--- Frontend/sitrep/src/components/StackedBarChart.js
+++ Frontend/sitrep/src/components/StackedBarChart.js
@@ -1,3 +1,4 @@
+
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -17,14 +18,51 @@ ChartJS.register(
     Legend
 );
 
-    
+const maxData = Array.from({ length: 4 }, () => Math.floor(Math.random() * 40));
+
+
+export const data = {
+    labels: [""],
+    datasets: [
+        {
+
+            stack: 'Stack 0',
+            backgroundColor: ["#18de41"],
+            label: 'Open',
+            data: [maxData[0]]
+        },
+        {
+
+            stack: 'Stack 0',
+            label: 'Closed',
+            backgroundColor: ["#238fde"],
+            data: [maxData[1]]
+        }
+        , {
+
+            stack: 'Stack 0',
+            label: 'In Progress',
+            backgroundColor: ["#D73A4A"],
+            data: [maxData[2]]
+        },
+        {
+
+            stack: 'Stack 0',
+            label: 'Resolved',
+            backgroundColor: ["#A371F7"],
+            data: [maxData[3]]
+        }
+    ],
+}
+
+
 export const options = {
-    tooltips: {enabled: false},
-    hover: {mode: null},
+    tooltips: { enabled: false },
+    hover: { mode: null },
     indexAxis: 'y',
     maintainAspectRatio: false,
     plugins: {
-        legend: {display: false},
+        legend: { display: false },
         title: {
             display: true,
             text: 'Ticket Status Ratio',
@@ -40,13 +78,16 @@ export const options = {
         x: {
             stacked: true,
             orientation: 'horizontal',
-            ticks: {color:"#343a40"},
-            max : 60,
+            ticks: { color: "#343a40" },
+            maxStepSize: 1,
+            max: maxData.reduce((a, b) => a + b, 0),
+
         },
         y: {
-            ticks: {color:"#343a40"},
+            ticks: { color: "#343a40" },
             stacked: true,
             orientation: 'horizontal',
+            stepSize: 1,
         },
     },
 };
