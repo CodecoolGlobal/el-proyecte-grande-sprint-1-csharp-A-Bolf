29a787d36c03ad76431a05ce4c978fbf84d89733
A-Bolf <hosc19@gmail.com>
2022-08-23 13:28:53 +0200

    add data model for Projects

--- Backend/SitRep/Models/Project.cs
+++ Backend/SitRep/Models/Project.cs
@@ -2,5 +2,7 @@ namespace SitRep.Models;
 
 public class Project
 {
-    
+    public int Id;
+    public string Name { get; set; }
+    public List<Ticket> Tickets { get; set; } = new List<Ticket>();
 }
