a345c92e1231f6cd861c9c1debed0892b1f8266e
A-Bolf <hosc19@gmail.com>
2022-09-06 19:55:50 +0200

    Implement Login and Registration forms on splash screen

--- Frontend/sitrep/src/components/LoginButton.js
+++ Frontend/sitrep/src/components/LoginButton.js
@@ -1,20 +1,39 @@
 import { useState } from "react";
 import Collapse from "@mui/material/Collapse";
 import Login from "./Login/Login";
+import Register from "./Registration/Register"
+import Stack from '@mui/material/Stack';
 const LoginButton = () => {
-  let [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+const [showRegister,setShowRegister]=useState(false)
   return (
     <>
+    <Stack direction="row"  spacing={5}>
       <button
         className="btn btn-primary"
         onClick={() => {
           setOpen(!open);
+          if(showRegister){
+          setShowRegister(false)}
         }}
       >
         {open ? "Nevermind" : "Login"}
       </button>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setShowRegister(!showRegister);
+          if (open){setOpen(false)}
+        }}
+      >
+        {showRegister ? "Nevermind" : "Register"}
+      </button>
+      </Stack>
       <Collapse in={open}>
-        <Login />
+        <Login/>
+      </Collapse>
+      <Collapse in={showRegister}>
+        <Register/>
       </Collapse>
     </>
   );
