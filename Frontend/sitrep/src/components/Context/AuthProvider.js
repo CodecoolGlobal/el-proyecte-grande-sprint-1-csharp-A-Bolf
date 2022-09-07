068f24d28f1878fbb6231fbddfd8c8c391d26d91
A-Bolf <hosc19@gmail.com>
2022-09-07 13:09:15 +0200

    add validation for registration fields, add automatic login after registration, fix redirect after login

--- Frontend/sitrep/src/components/Context/AuthProvider.js
+++ Frontend/sitrep/src/components/Context/AuthProvider.js
@@ -1,12 +1,68 @@
 import React, { createContext, useState } from "react";
+import axios from "axios";
+import { API_ENDPOINT } from "../../App";
+import { useNavigate } from "react-router";
 
 const AuthContext = createContext({});
+let navigate = useNavigate;
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+  const [errMsg, setErrMsg] = useState("");
 
+  const registerFetch = (data) => {
+    if (data.get("password") === "" || data.get("username") === "") {
+      setErrMsg("All Fields Are Required");
+      return;
+    }
+    if (data.get("password") !== data.get("password-confirm")) {
+      setErrMsg("Password Fields Must Match");
+      return;
+    }
+    axios
+      .post(`${API_ENDPOINT}/api/Auth/register`, {
+        userName: data.get("username"),
+        password: data.get("password"),
+      })
+      .then((res) => {
+        console.log(res.data);
+        loginFetch(data.get("username"), data.get("password"));
+      })
+      .catch((error) => {
+        if (error.response) {
+          setErrMsg("Username Already Taken");
+        }
+      });
+  };
+
+  const loginFetch = (username, password) => {
+    axios
+      .post(`${API_ENDPOINT}/api/Auth/login`, {
+        userName: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        let accessToken = res?.data;
+        setAuth({ username, password, accessToken });
+        navigate("/app/dashboard");
+      })
+      .catch((error) => {
+        if (!error.response) {
+          setErrMsg("No Server Response");
+        } else if (error.response.status === 401) {
+          setErrMsg("Unauthorized");
+        } else if (error.response.status === 400) {
+          setErrMsg("User Not Found!");
+        } else {
+          setErrMsg("Login Failed");
+        }
+      });
+  };
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider
+      value={{ auth, setAuth, loginFetch, registerFetch, errMsg, setErrMsg }}
+    >
       {children}
     </AuthContext.Provider>
   );
