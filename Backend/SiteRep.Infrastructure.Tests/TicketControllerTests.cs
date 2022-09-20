9c2fb3d74b8aa882d6db40e49dce6fdb73ffd0ec
Fehér Tamás <85102638+thomaswhite-mentor@users.noreply.github.com>
2022-09-20 16:08:52 +0200

    Add new ticket test

--- Backend/SiteRep.Infrastructure.Tests/TicketControllerTests.cs
+++ Backend/SiteRep.Infrastructure.Tests/TicketControllerTests.cs
@@ -1,28 +1,23 @@
+using FluentAssertions;
 using Newtonsoft.Json;
 using SiteRep.Infrastructure.Tests.Common;
 using SitRep.Models;
 
 namespace SiteRep.Infrastructure.Tests
 {
-    public class Tests
+    public class TicketControllerTests :BaseTest
     {
-        private readonly HttpClient _client;
-        public Tests()
-        {
-            TestingWebAppFactory<Program> factory = new TestingWebAppFactory<Program>();
-            _client = factory.CreateClient();
-        }
-
         [Test]
-        public async Task GetAllTicketTest()
+        public async Task Should_retrives_all_tickest()
         {
-            var response = await _client.GetAsync("/api/ticket");
+            var response = await Client.GetAsync("/api/ticket");
             //Assert
             response.EnsureSuccessStatusCode();
 
             var responseString = await response.Content.ReadAsStringAsync();
             var actual = JsonConvert.DeserializeObject<List<Ticket>>(responseString);
-            Assert.IsTrue(actual.Count > 0);
+            var expected = JsonConvert.DeserializeObject<List<Ticket>>(GetDataStructureFromJson("Resources.Ticket.alltickets.json"));
+            Assert.That(actual.Count, Is.EqualTo(expected.Count));
 
         }
     }
